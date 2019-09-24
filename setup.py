0b20bc5f1bab431e696d3a9114ef86eb484bfa00
lavanyanemani1996 <s6lanema@uni-bonn.de>
2019-09-24 18:28:44 +0200

    Adding setup.py file for installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='clumo',
+      version='0.0',
+      description='Mock galaxy cluster generation tool',
+      url='https://github.com/lavanyanemani1996/clumo.git',
+      install_requires=['numpy', 'math', 'astropy', 'hmf', 'scipy','random',
+			'time', 'multiprocessing', 'itertools','xspec'],
+      packages=['clumo'],
+      zip_safe=False)
